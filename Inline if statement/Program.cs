6cef1aeec8c612341378b5343bfbe549971b6c03
Sandra <snelson3671@yahoo.com>
2018-07-30 10:19:38 -0500

    Add project files.

---
+++ Inline if statement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inline_if_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
